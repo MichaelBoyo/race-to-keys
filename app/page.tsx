b449304f1555623899573528dbfefe1d9e7d8d7b
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-11-04 13:18:21 +0100

    twiter provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="">
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session?.user?.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </main>
   );
 }
