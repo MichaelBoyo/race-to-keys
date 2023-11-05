1ec45b2c1f510ca4d6865eb905cb60e394850f00
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-11-05 13:09:45 +0100

    alert

--- components/BuyAndSellClient.tsx
+++ components/BuyAndSellClient.tsx
@@ -2,10 +2,11 @@
 
 import { buyKeys, sellKeys } from "@/lib/contract";
 import { User } from "@/lib/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
 import DropDown from "./DropDown";
-
+import { useRouter } from "next/navigation";
+import { FaCheckSquare } from "react-icons/fa";
 type Props = {
   keySubjects: {
     address: string;
@@ -16,16 +17,46 @@ type Props = {
 export const BuyAndSellClient = ({ keySubjects, user }: Props) => {
   const [amount, setAmount] = useState(10);
   const [address, setAddress] = useState(keySubjects[0].address);
+  const router = useRouter();
+
+  const [alert, setAlert] = useState({
+    open: false,
+    message: "",
+  });
   const handleSubmit = async (buy?: boolean) => {
     if (buy) {
       await buyKeys(user, address, amount);
     } else {
       await sellKeys(user, address, amount);
     }
+    router.refresh();
+    setAlert({
+      open: true,
+      message: "Transaction successfull",
+    });
   };
+  useEffect(() => {
+    if (!alert.open) return;
+    const interval = setInterval(() => {
+      setAlert({
+        open: false,
+        message: "",
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [alert.open]);
 
   return (
-    <div className="  flex flex-col ">
+    <div className="  flex flex-col  ">
+      {alert.open ? (
+        <div className="absolute transition-all duration-300 flex p-5  w-screen justify-center items-center left-0 top-0 ">
+          <div className="alert w-max alert-success  ">
+            <FaCheckSquare />
+            <span>{alert.message}</span>
+          </div>
+        </div>
+      ) : null}
       <input
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
@@ -42,9 +73,9 @@ export const BuyAndSellClient = ({ keySubjects, user }: Props) => {
       <div className="flex gap-4">
         <button onClick={() => handleSubmit(true)} className="btn btn-accent">
           Buy
-          <p className="border gap-1 w-max  p-1 rounded-full bg-accent text-black font-semibold">
+          {/* <p className="border gap-1 w-max  p-1 rounded-full bg-accent text-black font-semibold">
             <FaArrowTrendUp />
-          </p>
+          </p> */}
         </button>
 
         <button onClick={() => handleSubmit()} className="btn btn-error">
--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -11,7 +11,7 @@ export default function DropDown({ items, selected, setSelected }: Props) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-base-100 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-base-100 px-3 py-2 text-sm font-semibold  shadow-sm ring-1 ring-inset  ">
           <p className="text-ellipsis overflow-hidden max-w-full">{selected}</p>
           <ChevronDownIcon className="-mr-1 h-5 w-5 " aria-hidden="true" />
         </Menu.Button>
@@ -35,7 +35,7 @@ export default function DropDown({ items, selected, setSelected }: Props) {
                     onClick={() => setSelected(item)}
                     href="#"
                     className={cx(
-                      active ? "bg-gray-400" : "",
+                      active ? "border border-gray-400" : "",
                       "block px-4 py-2 text-sm text-ellipsis overflow-hidden max-w-full"
                     )}
                   >
