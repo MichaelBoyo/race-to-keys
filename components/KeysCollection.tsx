0d9cda4083d71c0f92dad14a55ec9734d20b0592
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-11-11 10:39:22 +0100

    rm pg

--- components/KeysCollection.tsx
+++ components/KeysCollection.tsx
@@ -1,34 +1,30 @@
-"use client";
-import { useState } from "react";
-import { Pagination } from "./Pagination";
-import AddressStat from "./AddressStat";
+import { getKeyBalance } from "@/lib/contract";
+import { FcKey } from "react-icons/fc";
+import Stat from "./Stat";
 
 type Props = {
   addresses: string[];
   wallet_address: string;
 };
 const KeysCollection = ({ addresses, wallet_address }: Props) => {
-  const [page, setPage] = useState(1);
-  const size = 14;
-
-  const totalPages = addresses.length / size;
   return (
     <div className="flex flex-col gap-3 ">
       <h1 className="font-semibold text-3xl">Your Key Collection </h1>
       <div className="grid grid-cols-6 gap-5 max-h-[50vh] custom-scrollbar overflow-auto">
-        {addresses
-          .slice((page - 1) * size, (page - 1) * size + size)
-          .map((address, id) => {
-            return (
-              <AddressStat
-                key={id}
-                wallet_address={wallet_address}
-                address={address}
-              />
-            );
-          })}
+        {addresses.map(async (address, id) => {
+          const bal = await getKeyBalance(wallet_address, address);
+
+          return (
+            <Stat
+              key={id}
+              icon={<FcKey className="p-1" />}
+              sub="keys"
+              value={`${bal} `}
+              header={address}
+            />
+          );
+        })}
       </div>
-      <Pagination totalPages={totalPages} page={page} setPage={setPage} />
     </div>
   );
 };
